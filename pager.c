198ad93ffa4752e26d7c331d4b5a3c6aca2ec5fa
Pavel Stehule <pavel.stehule@gooddata.com>
2017-08-20 16:02:25 +0200

    tuning style 10

--- pager.c
+++ pager.c
@@ -672,7 +672,7 @@ initialize_color_pairs(int theme)
 
 		init_pair(2, COLOR_WHITE, COLOR_BLUE);
 		init_pair(3, COLOR_WHITE, COLOR_BLACK);
-		init_pair(4, COLOR_GREEN, COLOR_CYAN);
+		init_pair(4, COLOR_WHITE, COLOR_CYAN);
 		init_pair(5, COLOR_WHITE, COLOR_BLUE);
 		init_pair(6, COLOR_WHITE, COLOR_BLUE);
 		init_pair(8, COLOR_WHITE, COLOR_BLUE);
