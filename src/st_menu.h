d464408e25d0f1cd4d6a63478d46bedf9770da67
Pavel Stehule <pavel.stehule@gmail.com>
2019-04-27 21:38:39 +0200

    solaris compilation fix

--- src/st_menu.h
+++ src/st_menu.h
@@ -1,5 +1,22 @@
 #include <ncurses.h>
-#include <panel.h>
+
+#if defined HAVE_NCURSESW_CURSES_H
+#include <ncursesw/curses.h>
+#elif defined HAVE_NCURSESW_H
+#include <ncursesw.h>
+#elif defined HAVE_NCURSES_CURSES_H
+#include <ncurses/curses.h>
+#elif defined HAVE_NCURSES_H
+#include <ncurses.h>
+#elif defined HAVE_CURSES_H
+#include <curses.h>
+#else
+/* fallback */
+#include <ncurses/ncurses.h>
+#endif
+
+
+//#include <panel.h>
 
 #ifndef _ST_MENU_H
 
