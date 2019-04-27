7605e8f0e0a37fe5f2708a79e1deea1bb3e31786
Pavel Stehule <pavel.stehule@gmail.com>
2019-04-27 21:29:53 +0200

    some header's fixes for solaris

--- src/print.c
+++ src/print.c
@@ -11,7 +11,21 @@
  *-------------------------------------------------------------------------
  */
 
+#if defined HAVE_NCURSESW_CURSES_H
+#include <ncursesw/curses.h>
+#elif defined HAVE_NCURSESW_H
+#include <ncursesw.h>
+#elif defined HAVE_NCURSES_CURSES_H
+#include <ncurses/curses.h>
+#elif defined HAVE_NCURSES_H
+#include <ncurses.h>
+#elif defined HAVE_CURSES_H
 #include <curses.h>
+#else
+/* fallback */
+#include <ncurses/ncurses.h>
+#endif
+
 #include <stdlib.h>
 #include <string.h>
 
