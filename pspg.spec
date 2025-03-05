7ebf672f90c171cb8b098aa9efed9aa7fee6d30c
okbob@github.com <pavel.stehule@gmail.com>
2025-03-05 19:38:52 +0100

    prepare for 5.8.8

--- pspg.spec
+++ pspg.spec
@@ -3,7 +3,7 @@
 
 Summary: 	pspg: a unix pager optimized for psql
 Name: 		pspg
-Version: 	5.8.7
+Version: 	5.8.8
 Release: 	0%{?dist}
 License: 	BSD
 Group: 		Development/Tools
--- src/pspg.h
+++ src/pspg.h
@@ -35,7 +35,7 @@
 #define			FILE_TSV			2
 #define			FILE_MATRIX			3
 
-#define PSPG_VERSION "5.8.7"
+#define PSPG_VERSION "5.8.8"
 
 /* GNU Hurd does not define MAXPATHLEN */
 #ifndef MAXPATHLEN
